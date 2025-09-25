04facbf9757299a7904ce17a2e6bc2be42ebd0c3
krishnamurthypranesh <pk96ishere@gmail.com>
2025-09-25 22:47:06 +0200

    Fix issue with lualine

--- config.nvim/lua/sinistersquirrel/lazy/lualine.lua
+++ config.nvim/lua/sinistersquirrel/lazy/lualine.lua
@@ -6,7 +6,7 @@ return {
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filname'},
+                lualine_c = {{'filename', path=1}},
                 lualine_x = { 'filetype', 'fileformat', 'encoding'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'},
